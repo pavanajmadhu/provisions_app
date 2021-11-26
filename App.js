d53c68d4cb7af10ddf72f4c24e163b14c13330b8
pavanajmadhu <81358710+pavanajmadhu@users.noreply.github.com>
2021-11-26 14:02:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import GroceriesScreen  from './screens/groceriesScreen'
+import AdministratorLoginScreen from './screens/loginAdministratorScreen'
+import AddGroceries from './screens/addGroceriesScreen'
+import AddNewGroceries from './screens/addNewGroceriesScreen'
+
+import {createSwitchNavigator,createAppContainer} from 'react-navigation'
+import LoginScreen from './screens/loginScreen';
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
+firebase.initializeApp(firebaseConfig)
+export default class App extends React.Component {
+  render(){
+  return (
+<AppContainer/>  
+  );
+  }
+}
+
+
+const SwitchNavigator =createSwitchNavigator({
+  loginScreen:LoginScreen,
+  administratorLoginScreen:AdministratorLoginScreen,
+  AddGroceries:AddGroceries,
+  groceriesScreen:GroceriesScreen,
+  AddNewGroceries:AddNewGroceries
+  })
+  
+  
+  const AppContainer = createAppContainer(SwitchNavigator)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCZ5wPex6m6EllAg5unjpRxvwjKZVHj1k4",
+    authDomain: "provisions-app.firebaseapp.com",
+    projectId: "provisions-app",
+    storageBucket: "provisions-app.appspot.com",
+    messagingSenderId: "880848362501",
+    appId: "1:880848362501:web:a2add76a55e3cc029d506c"
+  };
